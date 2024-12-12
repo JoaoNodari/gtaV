7389b0c37e90c810c93939ebb8fce6dc9054c40b
João Nodari <joaonodari2017@hotmail.com>
2024-12-11 22:05:52 -0300

    Finalizando JS.

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,6 @@ const botaoPlaraforma = document.querySelector(".btn-plataforma")
 const abrirPlataforma = document.querySelector(".btn-plataforma .plataformas")
 
 botaoPlaraforma.addEventListener("click", () => {
-
-    let verificarTag = abrirPlataforma.classList.contains('ativo');
-
-    if(verificarTag) {
-        abrirPlataforma.classList.remove('ativo')
-    } else {
-        abrirPlataforma.classList.add('ativo')
-    }
+    abrirPlataforma.classList.toggle('ativo')
 })
 
